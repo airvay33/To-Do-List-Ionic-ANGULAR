f984a54c3d041a40aeaf129503b42f0db4c9611e
Airvay <60217351+airvay33@users.noreply.github.com>
2020-03-10 10:33:44 +0100

    Mise à jour de l'ensemble de ces pages pour la réalisation du projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,27 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCrDU5ysZnzQ4dlh-8TZFqGOeKCL7e-6VY",
+  authDomain: "monassmat33.firebaseapp.com",
+  databaseURL: "https://monassmat33.firebaseio.com",
+  projectId: "monassmat33",
+  storageBucket: "monassmat33.appspot.com",
+  messagingSenderId: "985228298455",
+  appId: "1:985228298455:web:56373b1b8e45ce22dac892",
+  measurementId: "G-SNQ21H5NQW"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), 
+  AppRoutingModule, AppRoutingModule,
+  AngularFireModule.initializeApp(firebaseConfig),
+  AngularFireDatabaseModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  
 })
 export class HomePage {
+  currentDate: string;
+  myTask: string;
+  addTask: boolean;
+  tasks = [];
 
-  constructor() {}
-
+  constructor(
+      public afDB: AngularFireDatabase,
+      ) {
+    const date = new Date();
+    const options = { weekday: 'long', month: 'long', day: 'numeric' };
+    this.currentDate = date.toLocaleDateString('fr-FR', options);
+    this.getTasks();
+  }
+  addTaskToFirebase() {
+    this.afDB.list('todolistmam33/').push({
+      text: this.myTask,
+      date: new Date().toISOString(),
+      checked: false
+    });
+    this.showForm();
+  }
+  showForm() {
+    this.addTask = !this.addTask;
+    this.myTask = '';
+  }
+  getTasks() {
+    this.afDB.list('todolistmam33/').snapshotChanges(['child_added', 'child_removed']).subscribe(actions => {
+      this.tasks = [];
+      actions.forEach(action => {
+        this.tasks.push({
+          key: action.key,
+          text: action.payload.exportVal().text,
+          hour: action.payload.exportVal().date.substring(11, 16),
+          checked: action.payload.exportVal().checked
+        });
+      });
+    });
+  }
+  changeCheckState(task: any) {
+    console.log('checked: ' + task.checked);
+    this.afDB.object('todolistmam33' + task.key + '/checked/').set(task.checked);
+  }
+  deleteTask(task: any) {
+    this.afDB.list('todolistmam33').remove(task.key);
+  }
 }
